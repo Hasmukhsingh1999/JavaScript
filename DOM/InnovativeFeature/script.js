59918db13f4d7838bf764559bf5d13294132479e
Hasmukh Singh <94060756+Hasmukhsingh1999@users.noreply.github.com>
2022-12-10 20:00:29 +0530

    Add files via upload

--- DOM/InnovativeFeature/script.js
+++ DOM/InnovativeFeature/script.js
@@ -1,57 +1,72 @@
 var databases = [
-    {name:"Sarah Logan",status:"Strangers",img:"https://i.pinimg.com/564x/e3/3c/ef/e33ceff89de6fdea10155eb5096ea148.jpg",justify:"Add Friend"},
+    {name:"Sarah Logan",status:"Strangers",img:"https://i.pinimg.com/564x/e3/3c/ef/e33ceff89de6fdea10155eb5096ea148.jpg"},
     
-    {name:"Katie Elizabeth",status:"Stargers",img:"https://i.pinimg.com/564x/1a/0a/c6/1a0ac6a684121b2e1d92d29449edd59b.jpg",justify:"Add Friend"},
+    // {name:"Katie Elizabeth",status:"Stargers",img:"https://i.pinimg.com/564x/1a/0a/c6/1a0ac6a684121b2e1d92d29449edd59b.jpg"},
     
-    {name:"Chelsea Marie" , status:"Strangers" ,img:"https://i.pinimg.com/564x/bd/5b/07/bd5b07f6b02b94d13ec1e5df1098a1e3.jpg",justify:"Add Friend"}
+    {name:"Chelsea Marie" , status:"Strangers" ,img:"https://i.pinimg.com/564x/bd/5b/07/bd5b07f6b02b94d13ec1e5df1098a1e3.jpg"}
 ]
-
-var btn = document.querySelector("button")
 
 
 function virtual(){
     var str = " ";
-   
-    databases.forEach(function(value,index){
+    databases.forEach((function(values,index){
         str += `<div id="card">
-        <img src="${value.img}" >
-        <h2>${value.name}</h2>
+        <img src="${values.img}" >
+        <h2>${values.name}</h2>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo consequuntur officiis vel ad?</p>
-        <h5>${value.status}</h5>
-        <button id="${index}">${value.justify}</button>
-    
-    </div>`
-    
-    });
-    document.querySelector("#cards").innerHTML = str;
-    
+        <h5 id=${values.status.split("  ").join(" ")}>${values.status}</h5>
+        <!-- <button>Add Friend</button> -->
+        <a href="#">
+            <span class="${values.status > "Friends" ? "Green" : "yellow" }" id="${index}">${values.status > "Starngers" ? "Add Friend" : "Cancel"}</span>
+            <div class="liquid"></div>
+        </a>
 
+    </div>`
+
+    }));
+    document.querySelector("#cards").innerHTML=str;
 }
 
 virtual();
-
-
-
-
-
+var flag = 0;
+// var timer;
 document.querySelector("#cards").addEventListener("click",function(details){
+    // databases[details.target.id].status = "Request Send";
+    // virtual();
+    // setTimeout(function(){
+    //     databases[details.target.id].status = "Friends";
+    //     virtual();
+
+    // },2000);
+
+    if(flag===0){
+        databases[details.target.id].status = "Sending...";
+        virtual();
+        setTimeout(function(){
+            databases[details.target.id].status = "Friends";
+            
+            virtual();
     
-    
-    databases[details.target.id].status = "Friend Request Send";
-    
-    virtual();
-    databases[details.target.id].justify = "Remove Friend";
-  
-    virtual();
-    setTimeout(function(){
+        },20);
+        flag=1;
         
-    });
+
+    }
+    else{
+        databases[details.target.id].status = "Removing Friend...";
+        virtual();
+        setTimeout(function(){
+            databases[details.target.id].status = "Strangers";
+            virtual();
     
+        },2000);
+        flag=0;
+        
 
-    
-
-
+    }
 });
+
+
 
 
 
